d90c47782fa1358310998b6f157fb9081f28e677
Ajithxan <Ajithshanth.creativehub@gmail.com>
2023-07-24 17:02:33 +0530

    mLive version change

--- meet.js
+++ meet.js
@@ -1,4 +1,4 @@
-const meetVersion = "1.3.1";
+const meetVersion = "1.3.3";
 const CDNlink = `https://cdn.jsdelivr.net/gh/Ajithxan/marketrix-live-${meetVersion}/`; //'http://localhost/creativehub/marketrix-live-1.7/'
 console.log(CDNlink);
 const startingTime = new Date().getTime();
